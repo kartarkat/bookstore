9bca148241beeeb8fe3c5a1d16f571be2198e8a7
Karthikeyan T <kartarkat@gmail.com>
2023-02-04 12:37:48 +0000

    fixed dot issue for netlify

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,19 +5,17 @@ import { FETCH_BOOKS_URL } from '../../Utils/api'
 
 function Home() {
   const [data, setData] = useState({})
-
+  
 
   const fetchBooks = (e) => {
-    fetch(`${FETCH_BOOKS_URL}${e.target.value}`).
-      then((res) => res.json()).
-      then(d => setData(d))
+    fetch(`${FETCH_BOOKS_URL}${e.target.value}`).then((res) => res.json()).then(d => setData(d))
   }
 
   const handleOnChange = DebounceHelper((e) => fetchBooks(e));
-
+  
   return (
     <div className={styles.input}>
-      <div className={styles.heading}>search books here</div>
+      <div className={styles.heading}>search book here</div>
       <input onChange={e => handleOnChange(e)} />
       {
         Array.isArray(data?.items) &&
